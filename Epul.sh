919baa5a18203eb11d2e8ac92c5f524eb4116e21
AdviandraHakim <trezuno66@gmail.com>
2024-11-19 11:27:20 +0700

    Add files via upload

--- Epul.sh
+++ Epul.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Otomasi Dimulai
+echo "Otomasi WaK"
+
+#install sshpass
+sudo apt install sshpass 
+
 # Repo Kartolo
 cat <<EOF | sudo tee /etc/apt/sources.list
 deb http://kartolo.sby.datautama.net.id/ubuntu/ focal main restricted universe multiverse
@@ -26,32 +32,33 @@ network:
      id: 10
      link: eth1
      addresses:
-      - 192.168.22.1/24
+      - 192.168.20.1/24
 EOT
 netplan apply
 
+#install isc-dhcp-server
 sudo apt install isc-dhcp-server 
 
 # Dhcp
 sudo bash -c 'cat > /etc/dhcp/dhcpd.conf' << EOF
 # A slightly different configuration for an internal subnet.
-subnet 192.168.22.0 netmask 255.255.255.0 {
-  range 192.168.22.2 192.168.22.254;
+subnet 192.168.20.0 netmask 255.255.255.0 {
+  range 192.168.20.2 192.168.20.254;
   option domain-name-servers 8.8.8.8;
 #  option domain-name "internal.example.org";
   option subnet-mask 255.255.255.0;
-  option routers 192.168.22.1;
-  option broadcast-address 192.168.22.255;
+  option routers 192.168.20.1;
+  option broadcast-address 192.168.20.255;
   default-lease-time 600;
   max-lease-time 7200;
 }
 EOF
 
-# Isc-dhcp-server
+# Config isc-dhcp-server
 echo 'INTERFACESv4="eth1.10"' > /etc/default/isc-dhcp-server
 systemctl restart isc-dhcp-server
 
-# ip forward
+# Ip forward
 sudo /etc/sysctl.conf
 net.ipv4.ip_forward=1
 
@@ -59,5 +66,3 @@ net.ipv4.ip_forward=1
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 sudo apt install iptables-persistent 
 
-#install sshpass
-sudo apt install sshpass 
