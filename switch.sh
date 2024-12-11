fa9e6a17ec86a4d34d04adf91d650b7788f4a3d7
AdviandraHakim <trezuno66@gmail.com>
2024-12-11 20:29:40 +0700

    Add files via upload

---
+++ switch.sh
@@ -0,0 +1,24 @@
+# Konfigurasi Cisco
+echo -e "${GREEN}${PROGRES[10]}${NC}"
+CISCO_IP="192.168.195.134"
+CISCO_PORT="30013"
+expect <<EOF > /dev/null 2>&1
+spawn telnet $CISCO_IP $CISCO_PORT
+set timeout 22
+
+expect ">" { send "enable\r" }
+expect "#" { send "configure terminal\r" }
+expect "(config)#" { send "interface Ethernet0/1\r" }
+expect "(config-if)#" { send "switchport mode access\r" }
+expect "(config-if)#" { send "switchport access vlan 10\r" }
+expect "(config-if)#" { send "no shutdown\r" }
+expect "(config-if)#" { send "exit\r" }
+expect "(config)#" { send "interface Ethernet0/0\r" }
+expect "(config-if)#" { send "switchport trunk encapsulation dot1q\r" }
+expect "(config-if)#" { send "switchport mode trunk\r" }
+expect "(config-if)#" { send "no shutdown\r" }
+expect "(config-if)#" { send "exit\r" }
+expect "(config)#" { send "exit\r" }
+expect "#" { send "exit\r" }
+expect eof
+EOF
