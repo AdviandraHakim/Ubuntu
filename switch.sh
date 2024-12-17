9753f706075d40de0f5715041abc206bbfb6da8f
AdviandraHakim <trezuno66@gmail.com>
2024-12-17 16:32:12 +0700

    Update switch.sh

--- switch.sh
+++ switch.sh
@@ -9,15 +9,15 @@ spawn telnet $CISCO_IP $CISCO_PORT
 set timeout 10
 
 # Masuk ke mode enable
-echo "[INFO] Masuk ke mode enable."
+puts "[INFO] Masuk ke mode enable."
 expect ">" { send "enable\r" }
 
 # Masuk ke konfigurasi terminal
-echo "[INFO] Masuk ke konfigurasi terminal."
+puts "[INFO] Masuk ke konfigurasi terminal."
 expect "#" { send "configure terminal\r" }
 
 # Konfigurasi interface Ethernet0/1
-echo "[INFO] Mengonfigurasi interface Ethernet0/1 sebagai access mode."
+puts "[INFO] Mengonfigurasi interface Ethernet0/1 sebagai access mode."
 expect "(config)#" { send "interface Ethernet0/1\r" }
 expect "(config-if)#" { send "switchport mode access\r" }
 expect "(config-if)#" { send "switchport access vlan 10\r" }
@@ -25,7 +25,7 @@ expect "(config-if)#" { send "no shutdown\r" }
 expect "(config-if)#" { send "exit\r" }
 
 # Konfigurasi interface Ethernet0/0
-echo "[INFO] Mengonfigurasi interface Ethernet0/0 sebagai trunk mode."
+puts "[INFO] Mengonfigurasi interface Ethernet0/0 sebagai trunk mode."
 expect "(config)#" { send "interface Ethernet0/0\r" }
 expect "(config-if)#" { send "switchport trunk encapsulation dot1q\r" }
 expect "(config-if)#" { send "switchport mode trunk\r" }
@@ -33,7 +33,7 @@ expect "(config-if)#" { send "no shutdown\r" }
 expect "(config-if)#" { send "exit\r" }
 
 # Keluar konfigurasi
-echo "[INFO] Menyelesaikan konfigurasi dan keluar."
+puts "[INFO] Menyelesaikan konfigurasi dan keluar."
 expect "(config)#" { send "exit\r" }
 expect "#" { send "exit\r" }
 expect eof
