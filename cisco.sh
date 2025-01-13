067d0e6524c86fff4b8c79a60dcf7087103c02ca
AdviandraHakim <trezuno66@gmail.com>
2025-01-13 14:30:35 +0700

    Update cisco.sh

--- cisco.sh
+++ cisco.sh
@@ -1,6 +1,8 @@
+
+
 #!/bin/bash
 
-CISCO_IP="192.168.195.134"  # IP perangkat Cisco    
+CISCO_IP="192.168.195.134"  # IP perangkat Cisco
 CISCO_PORT="30013"
 
 # Konfigurasi Cisco: VLAN, Trunking, dan Akses Port DHCP
@@ -10,18 +12,22 @@ send \"enable\r\"
 expect \"#\"
 send \"configure terminal\r\"
 expect \"(config)#\"
+
+# Membuat VLAN 10
 send \"vlan 10\r\"
 expect \"(config-vlan)#\"
 send \"name VLAN10\r\"
 expect \"(config-vlan)#\"
 send \"exit\r\"
+
+# Mengonfigurasi interface trunk (port e0/0)
 send \"interface e0/0\r\"  
 expect \"(config-if)#\"
 send \"switchport trunk encapsulation dot1q\r\"
 expect \"(config-if)#\"
 send \"switchport mode trunk\"
-expect \"(config-if)#\"
-send \"exit\r\"
+
+# Mengonfigurasi port akses VLAN 10 (kabel DHCP)
 send \"interface e0/1\r\"  
 expect \"(config-if)#\"
 send \"switchport mode access\r\"
@@ -29,6 +35,8 @@ expect \"(config-if)#\"
 send \"switchport access vlan 10\r\"
 expect \"(config-if)#\"
 send \"exit\r\"
+
+# Menyimpan konfigurasi
 send \"end\r\"
 expect \"#\"
 send \"write memory\r\"
