4e4ad41c90bd7d0888c1f130f5e70cdc2df475d6
AdviandraHakim <trezuno66@gmail.com>
2025-01-13 14:09:35 +0700

    Update cisco.sh

--- cisco.sh
+++ cisco.sh
@@ -10,22 +10,18 @@ send \"enable\r\"
 expect \"#\"
 send \"configure terminal\r\"
 expect \"(config)#\"
-
-# Membuat VLAN 10
 send \"vlan 10\r\"
 expect \"(config-vlan)#\"
 send \"name VLAN10\r\"
 expect \"(config-vlan)#\"
 send \"exit\r\"
-
-# Mengonfigurasi interface trunk (port e0/0)
 send \"interface e0/0\r\"  
 expect \"(config-if)#\"
 send \"switchport trunk encapsulation dot1q\r\"
 expect \"(config-if)#\"
 send \"switchport mode trunk\"
-
-# Mengonfigurasi port akses VLAN 10 (kabel DHCP)
+expect \"(config-if)#\"
+send \"exit\r\"
 send \"interface e0/1\r\"  
 expect \"(config-if)#\"
 send \"switchport mode access\r\"
@@ -33,8 +29,6 @@ expect \"(config-if)#\"
 send \"switchport access vlan 10\r\"
 expect \"(config-if)#\"
 send \"exit\r\"
-
-# Menyimpan konfigurasi
 send \"end\r\"
 expect \"#\"
 send \"write memory\r\"
